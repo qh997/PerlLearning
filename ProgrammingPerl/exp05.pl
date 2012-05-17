0884992f12dfdafc87f68d90dc4ea5abb2e9c2ec
Geng Shuang <gengs@neusoft.com>
2012-05-17 18:02:42 +0800

    Study PP EXP 05
    小引擎/能与不能/

--- ProgrammingPerl/exp05.pl
+++ ProgrammingPerl/exp05.pl
@@ -41,3 +41,105 @@ while (<FH>) {
 say "$frist | $last";
 
 # s/// 操作符（替换）
+$_ = '2581';
+s/(\d+)/sprintf("%#x", $1)/e;
+say;
+
+# 赋值并替换，修改副本的例子
+@oldhues = ('bluebird', 'bluegress', 'bluefish', 'the blues');
+for (@newhues = @oldhues) {s/blue/red/};
+say "@newhues";
+
+# 规范变量中的空白
+$string = '  Hello     World!  ';
+for ($string1 = $string) {
+    s/^\s+//;
+    s/\s+$//;
+    s/\s+/ /g;
+}
+say "[$string1]";
+
+# 另一种方法
+$string2 = join(' ', split ' ', $string);
+say "[$string2]";
+
+# 当全局替换不够“全局”时
+say '-' x 45;
+$_ = 1234567890.22332112;
+say;
+say "[$`]<($1)($2)>[$']" while s/(\d+)(\d{3})/$1,$2/;
+say;
+
+say '-' x 45;
+$_ = "\t\tHello\tworld!\t";
+say;
+say "[$`]<$&>[$']" while s/\t+/'['.'.' x (length($&) * 8 - length($') % 8).']'/e;
+say "$_";
+
+say '-' x 45;
+$_ = "this is(a lots (of (parenthese)s))";
+say;
+say "[$`]<$&>[$']" while s/\([^()]*\)//g;
+say;
+
+say '-' x 45;
+$_ = "Paris in THE THE THE THE spring.";
+say;
+say "[$`]<$&>[$']" while s/\b(\w+)\s+\1\b/$1/g;
+say;
+say '-' x 45;
+
+# 元字符和元符号
+$_ = "abc6gdef";
+/(??{5+1})/;
+say "[$`]<$&>[$']";
+
+# if...then...else
+/(?(?<=a)c|e)/;
+say "[$`]<$&>[$']";
+/(a).*(?(1)c|e)/;
+say "[$`]<$&>[$']";
+/(?(?{1})c|e)/;
+say "[$`]<$&>[$']";
+
+$_ = 'this is(a lots (of (parenthese)s (OK) ))';
+say "[$`]<$&>[$']" while /(\()?[^()]+(?(1)\))/g;
+
+# 顺序匹配
+$burglar = "Bilbo Baggins";
+while ($burglar =~ /b/gci) {
+    printf "Found a B at %d\n", pos($burglar) - 1;
+}
+while ($burglar =~ /i/gi) {
+    printf "Found a I at %d\n", pos($burglar) - 1;
+}
+
+# 离开之处：\G 断言
+$_ = "hello Perl hello world";
+/hello\s+(\w+)/g;
+say $1;
+say pos;
+say $1 if /\G\s+hello\s+(\w+)/;
+
+# 捕获
+$_ = "abcdefghijklmnopqrstuvwxyz";
+/(hi).*(stu)/;
+say "The entire matchbegan at $-[0] and ended at $+[0]";
+say "The frist  matchbegan at $-[1] and ended at $+[1]";
+say "The second matchbegan at $-[2] and ended at $+[2]";
+
+# 变量内插
+chomp($answer = <STDIN>);
+if    ("SEND"  =~ /^\Q$answer/i) {say "Action is send."}
+elsif ("STOP"  =~ /^\Q$answer/i) {say "Action is stop."}
+elsif ("ABORT" =~ /^\Q$answer/i) {say "Action is abort."}
+elsif ("LIST"  =~ /^\Q$answer/i) {say "Action is list."}
+elsif ("EDIT"  =~ /^\Q$answer/i) {say "Action is edit."}
+
+chomp($pattern = <STDIN>);
+$_ = "\t22342 i am here";
+say "[$`]<$&>[$']" if /$pattern/o;
+
+# 正则表达式编译器
+use re "debug";
+"Smeagol" =~ /^Sm(.*)g[aeiou]l$/;
