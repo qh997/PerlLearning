23cc5a6b9a31172d4c754afb669d03d9f20c2fc1
Geng Shuang <qh997@hotmail.com>
2012-05-28 22:13:10 +0800

    第四章 子例程引用与闭包

--- AdvancedPerlProgramming/exp04.pl
+++ AdvancedPerlProgramming/exp04.pl
@@ -36,6 +36,7 @@ ps;
 
 # 闭包的应用
 ## 将闭包用作“智能”回调
+=del
 use Tk;
 my $topwindow = MainWindow->new();
 CreatButton($topwindow, "hello");
@@ -50,3 +51,37 @@ sub CreatButton {
         '-command' => sub {say "Button $title pressed."},
     )->pack();
 }
+=cut
+
+## 迭代器与流
+sub even_number_printer_gen {
+    my $input = shift;
+    if ($input % 2) {$input++}
+    my $rs = sub {
+        say "$input";
+        $input += 2;
+    };
+    return $rs;
+}
+
+my $iterator = even_number_printer_gen(30);
+for (my $i = 0; $i < 10; $i++) {
+    &$iterator();
+}
+ps;
+
+### 随机数生成
+sub my_srand {
+    my $seed = shift;
+    my $rand = $seed;
+    return sub {$rand = ($rand * 21 + 1) % 1000}
+}
+
+my $random1 = my_srand(100);
+my $random2 = my_srand(1099);
+for (1..10) {
+    say &$random1().' '.$random2->();
+}
+ps;
+
+## 闭包与对象的对比
